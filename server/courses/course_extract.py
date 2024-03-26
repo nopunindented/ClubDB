f7d1630aadccee2a31ad43d864e2aaa0916fb59a
nopunindented <umerfiaz251@gmail.com>
2024-03-25 21:56:10 -0600

    "Rotating proxies set up"

--- server/courses/course_extract.py
+++ server/courses/course_extract.py
@@ -9,14 +9,14 @@ class CourseExtract():
         self.DRIVER_PATH = "C:/Users/umerf\Downloads\chrome-win64\chrome-win64\chrome.exe"
         self.list_of_courses = []
     
-    def getProxies():
-        r = requests.get('<https://free-proxy-list.net/>')
+    def getProxies(self):
+        r = requests.get('https://free-proxy-list.net/')
         soup = BeautifulSoup(r.content, 'html.parser')
         table = soup.find('tbody')
 
         proxies = []
         for row in table:
-            if row.find_all('td')[4].text == 'elite proxy':
+            if row.find_all('td')[4].text == 'elite proxy' and row.find_all('td')[5].text == 'yes':
                 proxy = ':'.join([row.find_all('td')[0].text, row.find_all('td')[1].text])
                 proxies.append(proxy)
             else:
@@ -26,9 +26,13 @@ class CourseExtract():
     
     def extract(self):
         list_of_proxies = self.getProxies()
-        
+
         options = Options()
-        driver = webdriver.Chrome()
+
+        for i in range(0, len(list_of_proxies)):
+            options.add_argument(f'--proxy-server={list_of_proxies[i]}')
+
+        driver = webdriver.Chrome(options=options)
         driver.get('https://apps.ualberta.ca/catalogue/course/ece/202')
 
     def run(self):

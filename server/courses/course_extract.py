5401e11f611c5b6f061c3c1d2d8ba447ee7d6570
nopunindented <umerfiaz251@gmail.com>
2024-03-26 23:26:04 -0600

    "For nano and normal compe underway"

--- server/courses/course_extract.py
+++ server/courses/course_extract.py
@@ -9,8 +9,13 @@ from selenium.webdriver.common.by import By
 
 class CourseExtract():
 
-    def __init__(self):
+    def __init__(self, study_program):
+
         self.list_of_courses = []
+        self.study_program = study_program
+        self.software_url = 'https://calendar.ualberta.ca/preview_program.php?catoid=39&poid=47959&returnto=12339'
+        self.compe_normal_url = 'https://calendar.ualberta.ca/preview_program.php?catoid=39&poid=47952&returnto=12339'
+        self.compe_nano_url = 'https://calendar.ualberta.ca/preview_program.php?catoid=39&poid=47954&returnto=12339'
     
     def getProxies(self):
         r = requests.get('https://free-proxy-list.net/')
@@ -45,18 +50,28 @@ class CourseExtract():
         }
 
         chrome_options = webdriver.ChromeOptions()
-
         driver = uc.Chrome(options=chrome_options, seleniumwire_options=seleniumwire_options)
-        driver.get('https://calendar.ualberta.ca/preview_program.php?catoid=39&poid=47959&returnto=12339')
+
+        url_to_search = ''
+        list_of_non_grp2s = []
+
+        list_of_non_grp2_compe_normal = ['ECE 203', 'ECE 302', 'ECE 340', 'ECE 304', 'ECE 342', 'ECE 410', 'ECE 492']
+        list_of_non_grp2_compe_nano = []
+        list_of_non_grp2_software = ['CMPUT 274', 'ECE 202', 'ECE 210', 'ENGG 299', 'MATH 201', 'MATH 209', 'CMPUT 272', 'CMPUT 275', 'ECE 212', 'ECE 240', 'PHYS 230', 'WKEXP 901', 'ECE 311', 'ECE 321', 'ECE 325', 'STAT 235', 'WKEXP 902', 'WKEXP 903', 'CMPUT 291', 'CMPUT 301', 'CMPUT 379', 'ECE 322', 'ECE 315', 'ECE 421', 'ECE 487', 'ENGG 404', 'WKEXP 904', 'WKEXP 905', 'ECE 420', 'ECE 422', 'ECE 493', 'ENGG 400']
+
+        if self.study_program == 'compe':
+            url_to_search = self.compe_normal_url
+
+        driver.get(url_to_search)
+
         h1 = driver.find_elements(By.CLASS_NAME, 'acalog-course')
-        list_of_non_grp2 = ['CMPUT 274', 'ECE 202', 'ECE 210', 'ENGG 299', 'MATH 201', 'MATH 209', 'CMPUT 272', 'CMPUT 275', 'ECE 212', 'ECE 240', 'PHYS 230', 'WKEXP 901', 'ECE 311', 'ECE 321', 'ECE 325', 'STAT 235', 'WKEXP 902', 'WKEXP 903', 'CMPUT 291', 'CMPUT 301', 'CMPUT 379', 'ECE 322', 'ECE 315', 'ECE 421', 'ECE 487', 'ENGG 404', 'WKEXP 904', 'WKEXP 905', 'ECE 420', 'ECE 422', 'ECE 493', 'ENGG 400']
 
         list_of_grp2 = []
         for i in range(0, len(h1)):
             split_elective = h1[i].text.split()
             try:
                 value = int(split_elective[1])
-                if (split_elective[0] + ' ' + split_elective[1]) not in list_of_non_grp2:
+                if (split_elective[0] + ' ' + split_elective[1]) not in list_of_non_grp2_software:
                     list_of_grp2.append(split_elective[0] + ' ' + split_elective[1])
             except ValueError:
                 pass
@@ -69,5 +84,5 @@ class CourseExtract():
         self.extract_group_two()
 
 if __name__ == "__main__":
-    extract_object = CourseExtract()
+    extract_object = CourseExtract('compe')
     extract_object.run()

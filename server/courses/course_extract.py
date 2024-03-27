3616307c1cec66510fe2965a20225ffe61e15357
nopunindented <umerfiaz251@gmail.com>
2024-03-26 23:13:24 -0600

    "Software group 2 electives extracted successfully"

--- server/courses/course_extract.py
+++ server/courses/course_extract.py
@@ -49,8 +49,20 @@ class CourseExtract():
         driver = uc.Chrome(options=chrome_options, seleniumwire_options=seleniumwire_options)
         driver.get('https://calendar.ualberta.ca/preview_program.php?catoid=39&poid=47959&returnto=12339')
         h1 = driver.find_elements(By.CLASS_NAME, 'acalog-course')
+        list_of_non_grp2 = ['CMPUT 274', 'ECE 202', 'ECE 210', 'ENGG 299', 'MATH 201', 'MATH 209', 'CMPUT 272', 'CMPUT 275', 'ECE 212', 'ECE 240', 'PHYS 230', 'WKEXP 901', 'ECE 311', 'ECE 321', 'ECE 325', 'STAT 235', 'WKEXP 902', 'WKEXP 903', 'CMPUT 291', 'CMPUT 301', 'CMPUT 379', 'ECE 322', 'ECE 315', 'ECE 421', 'ECE 487', 'ENGG 404', 'WKEXP 904', 'WKEXP 905', 'ECE 420', 'ECE 422', 'ECE 493', 'ENGG 400']
+
+        list_of_grp2 = []
         for i in range(0, len(h1)):
-            print(h1[i].text)
+            split_elective = h1[i].text.split()
+            try:
+                value = int(split_elective[1])
+                if (split_elective[0] + ' ' + split_elective[1]) not in list_of_non_grp2:
+                    list_of_grp2.append(split_elective[0] + ' ' + split_elective[1])
+            except ValueError:
+                pass
+
+        print(list_of_grp2)
+
         driver.quit()
 
     def run(self):

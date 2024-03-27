fb1a7ebe87216e81e23a942cd4d345d527179f04
nopunindented <umerfiaz251@gmail.com>
2024-03-26 23:33:27 -0600

    "Removed non group 2's automatically for nano, normal, and software"

--- server/courses/course_extract.py
+++ server/courses/course_extract.py
@@ -55,23 +55,30 @@ class CourseExtract():
         url_to_search = ''
         list_of_non_grp2s = []
 
-        list_of_non_grp2_compe_normal = ['ECE 203', 'ECE 302', 'ECE 340', 'ECE 304', 'ECE 342', 'ECE 410', 'ECE 492']
-        list_of_non_grp2_compe_nano = []
+        list_of_non_grp2_compe_normal = ['CMPUT 274', 'ECE 202', 'ECE 210', 'ENGG 299', 'MATH 201', 'MATH 209', 'CMPUT 272', 'CMPUT 275', 'ECE 212', 'ECE 240', 'PHYS 230', 'WKEXP 901', 'ECE 311', 'ECE 325', 'STAT 235', 'WKEXP 902', 'WKEXP 903', 'CMPUT 291', 'CMPUT 301', 'CMPUT 379', 'ECE 322', 'ECE 315', 'ECE 487', 'ENGG 404', 'WKEXP 904', 'WKEXP 905', 'ECE 420', 'ECE 422', 'ECE 493', 'ENGG 400','ECE 203', 'ECE 302', 'ECE 340', 'ECE 304', 'ECE 342', 'ECE 410', 'ECE 492']
+        list_of_non_grp2_compe_nano = ['CMPUT 274', 'ECE 202', 'ECE 210', 'ENGG 299', 'MATH 201', 'MATH 209', 'CMPUT 272', 'CMPUT 275', 'ECE 203', 'ECE 212', 'ECE 240', 'PHYS 230', 'WKEXP 901', 'ECE 302', 'ECE 311', 'ECE 325', 'WKEXP 902', 'WKEXP 903', 'CMPUT 291', 'ECE 304', 'ECE 342', 'ECE 410', 'ENGG 404', 'CMPUT 301', 'ECE 315', 'ECE 403', 'ECE 450', 'ECE 475', 'WKEXP 904', 'WKEXP 905', 'ECE 412', 'ECE 457', 'ECE 492', 'ENGG 400']
         list_of_non_grp2_software = ['CMPUT 274', 'ECE 202', 'ECE 210', 'ENGG 299', 'MATH 201', 'MATH 209', 'CMPUT 272', 'CMPUT 275', 'ECE 212', 'ECE 240', 'PHYS 230', 'WKEXP 901', 'ECE 311', 'ECE 321', 'ECE 325', 'STAT 235', 'WKEXP 902', 'WKEXP 903', 'CMPUT 291', 'CMPUT 301', 'CMPUT 379', 'ECE 322', 'ECE 315', 'ECE 421', 'ECE 487', 'ENGG 404', 'WKEXP 904', 'WKEXP 905', 'ECE 420', 'ECE 422', 'ECE 493', 'ENGG 400']
 
         if self.study_program == 'compe':
             url_to_search = self.compe_normal_url
+            list_of_non_grp2s = list_of_non_grp2_compe_normal
+        elif self.study_program == 'software':
+            url_to_search = self.software_url
+            list_of_non_grp2s = list_of_non_grp2_software
+        elif self.study_program == 'nano':
+            url_to_search = self.compe_nano_url
+            list_of_non_grp2s = list_of_non_grp2_compe_nano
 
         driver.get(url_to_search)
 
         h1 = driver.find_elements(By.CLASS_NAME, 'acalog-course')
-
         list_of_grp2 = []
+
         for i in range(0, len(h1)):
             split_elective = h1[i].text.split()
             try:
                 value = int(split_elective[1])
-                if (split_elective[0] + ' ' + split_elective[1]) not in list_of_non_grp2_software:
+                if (split_elective[0] + ' ' + split_elective[1]) not in list_of_non_grp2s:
                     list_of_grp2.append(split_elective[0] + ' ' + split_elective[1])
             except ValueError:
                 pass
@@ -84,5 +91,5 @@ class CourseExtract():
         self.extract_group_two()
 
 if __name__ == "__main__":
-    extract_object = CourseExtract('compe')
+    extract_object = CourseExtract('nano') # can put compe, software, or nano in the constructor
     extract_object.run()

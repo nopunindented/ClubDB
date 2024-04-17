48ec85ee2eb1e5986d0cf01a910d5fcb22e96eb3
nopunindented <umerfiaz251@gmail.com>
2024-04-16 22:45:53 -0600

    "vector db creation added"

--- server/courses/create_vectordb.py
+++ server/courses/create_vectordb.py
@@ -3,6 +3,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 import os
+from docx2pdf import convert
 
 def vectordb(file, directory):
     loader = PyPDFLoader(file)
@@ -20,5 +21,26 @@ def vectordb(file, directory):
 
     db = Chroma.from_documents(all_splits, embeddings, persist_directory = nested_directory_path)
 
-    return
+    return db
 
+if __name__ == "__main__":
+
+    directory_path = os.getcwd()
+
+    contents = os.listdir(directory_path)
+
+    for file in contents:
+        item_path = os.path.join(directory_path, file)
+
+        if os.path.isdir(item_path) and "cache" not in item_path:
+            directory_contents = os.listdir(item_path)
+
+            for i in range(0, len(directory_contents)):
+                if ".docx" in directory_contents[i]:
+                    nested_item_path = os.path.join(item_path, directory_contents[i])
+
+                    file_minus_extension = nested_item_path.split('.')
+                    pdf_file_path = file_minus_extension[0] + '.pdf'
+                    convert(nested_item_path, pdf_file_path)
+
+                    vectordb(pdf_file_path, item_path)
